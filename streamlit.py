03aeebde70929f9b8c59a3faf194e460b858a26e
User-CruZer <ahmadzakialbadri@gmail.com>
2024-11-15 17:54:13 +0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.sidebar.write("# About!")
+st.sidebar.write("waiting for season 2 for real") # Ketik disini mad
+
+st.title("Hello there")
+st.subheader("Welcome")
+st.subheader("To cancer detection made by yukin dulu team")
+
+uploaded_files = st.file_uploader(
+    "insert an image", accept_multiple_files=True, type=["png", "jpg"]
+)
+
+submitter =  f"{uploaded_files}", st.button("submit image", use_container_width= True)
+
+
+# ChatGpt Masih Error
+chatgpt_url = "https://chat.openai.com/"
+iframe_code = f"""
+    <iframe src="{chatgpt_url}" width="100%" height="800px" frameborder="0"></iframe>
+"""
+components.html(iframe_code, height=200)
